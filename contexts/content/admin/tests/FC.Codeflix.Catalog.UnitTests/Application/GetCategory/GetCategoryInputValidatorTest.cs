728ace16c3754f53c97c35d469151bfdf6269195
Rafael Freitas <raffreitasx@gmail.com>
2025-04-03 08:37:42 -0300

    Add validation for GetCategoryInput with tests
    Updated project to include FluentValidation and MediatR.
    Added GetCategoryInputValidator to ensure Id is not empty.
    Created GetCategoryInputValidatorTest with tests for valid
    and invalid input scenarios.

---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,42 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Categories.GetCategory;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryInputValidatorTest
+{
+    private readonly GetCategoryTestFixture _fixture;
+    public GetCategoryInputValidatorTest(GetCategoryTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(ValidationOk))]
+    [Trait("Application", "GetCategory - Validator")]
+    public void ValidationOk()
+    {
+        var validInput = new GetCategoryInput(Guid.NewGuid());
+        var validator = new GetCategoryInputValidator();
+
+        var result = validator.Validate(validInput);
+
+        result.Should().NotBeNull();
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().HaveCount(0);
+    }
+
+    [Fact(DisplayName = nameof(InvalidWhenIdIsEmpty))]
+    [Trait("Application", "GetCategory - Validator")]
+    public void InvalidWhenIdIsEmpty()
+    {
+        var invalidInput = new GetCategoryInput(Guid.Empty);
+        var validator = new GetCategoryInputValidator();
+
+        var result = validator.Validate(invalidInput);
+
+        result.Should().NotBeNull();
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors[0].PropertyName.Should().Be("Id");
+        result.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
+    }
+}
