94ca1f72fdbf5b3b8c8d3f58157eb84fcfe9d5eb
Rafael Freitas <raffreitasx@gmail.com>
2025-04-07 06:38:45 -0300

    Add input validation for category updates
    Implemented `UpdateCategoryInputValidator` to ensure the `Id` property is not empty. Added unit tests in `UpdateCategoryInputValidatorTest` to verify validation behavior for both empty and valid GUID inputs.

---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryInputValidatorTest.cs
@@ -0,0 +1,42 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Categories.UpdateCategory;
+
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixtureCollection))]
+public class UpdateCategoryInputValidatorTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+    public UpdateCategoryInputValidatorTest(UpdateCategoryTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DontValidateWhenEmptyGuid))]
+    [Trait("Application", "UpdateCategoryInputValidator - Use Cases")]
+    public void DontValidateWhenEmptyGuid()
+    {
+        var input = _fixture.GetValidInput(Guid.Empty);
+        var validator = new UpdateCategoryInputValidator();
+
+        var result = validator.Validate(input);
+
+        result.Should().NotBeNull();
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
+    }
+
+    [Fact(DisplayName = nameof(ValidateWhenValid))]
+    [Trait("Application", "UpdateCategoryInputValidator - Use Cases")]
+    public void ValidateWhenValid()
+    {
+        var input = _fixture.GetValidInput();
+        var validator = new UpdateCategoryInputValidator();
+
+        var result = validator.Validate(input);
+
+        result.Should().NotBeNull();
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().HaveCount(0);
+    }
+}
