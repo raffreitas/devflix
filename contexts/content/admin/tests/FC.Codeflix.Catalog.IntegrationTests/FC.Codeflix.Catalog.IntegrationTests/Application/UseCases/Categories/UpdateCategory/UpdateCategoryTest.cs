871de931b5a9bd0d179757bacc0eb5729c2e5c35
Rafael Freitas <raffreitasx@gmail.com>
2025-04-09 20:39:12 -0300

    Add integration tests for UpdateCategory use case
    This commit introduces a comprehensive set of integration tests for the `UpdateCategory` use case in `UpdateCategoryTest.cs`. The tests cover scenarios such as handling category not found exceptions, updating categories, and validating edge cases with invalid inputs.
    
    Additionally, the `UpdateCategoryTestDataGenerator.cs` file is updated to provide necessary test data, while the `UpdateCategoryTestFixture.cs` file is enhanced with methods for generating valid and invalid `UpdateCategoryInput` objects. These changes significantly improve test coverage and ensure expected behaviors are validated.

---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.IntegrationTests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Categories/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,173 @@
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Application.UseCases.Categories.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Categories.UpdateCategory;
+using FC.Codeflix.Catalog.Domain.Entities;
+using FC.Codeflix.Catalog.Domain.Exceptions;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+
+using FluentAssertions;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTest(UpdateCategoryTestFixture fixture)
+{
+    [Fact(DisplayName = nameof(ThrowWhenCategoryNotFound))]
+    [Trait("Integration/Application", "UpdateCategory - Use Cases")]
+    public async Task ThrowWhenCategoryNotFound()
+    {
+        var dbContext = fixture.CreateDbContext();
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var input = fixture.GetValidInput();
+        var useCase = new UpdateCategoryUseCase(repository, unitOfWork);
+
+        var act = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await act.Should()
+            .ThrowAsync<NotFoundException>();
+    }
+
+    [Theory(DisplayName = nameof(UpdateCategory))]
+    [Trait("Integration/Application", "UpdateCategory - Use Cases")]
+    [MemberData(
+        nameof(UpdateCategoryTestDataGenerator.GetCategoriesToUpdate),
+        parameters: 5,
+        MemberType = typeof(UpdateCategoryTestDataGenerator)
+    )]
+    public async Task UpdateCategory(Category exampleCategory, UpdateCategoryInput input)
+    {
+        var dbContext = fixture.CreateDbContext();
+        await dbContext.AddRangeAsync(fixture.GetExampleCategoryList());
+        var trackingInfo = await dbContext.AddAsync(exampleCategory);
+        await dbContext.SaveChangesAsync();
+        trackingInfo.State = EntityState.Detached;
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var useCase = new UpdateCategoryUseCase(repository, unitOfWork);
+
+        CategoryModelOutput output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be((bool)input.IsActive!);
+        var dbCategory = await fixture
+           .CreateDbContext(preserveData: true)
+           .Categories.SingleAsync((x) => x.Id == input.Id, CancellationToken.None);
+        dbCategory.Should().NotBeNull();
+        dbCategory.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be((bool)input.IsActive);
+        dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+    }
+
+    [Theory(DisplayName = nameof(UpdateCategoryOnlyName))]
+    [Trait("Integration/Application", "UpdateCategory - Use Cases")]
+    [MemberData(
+    nameof(UpdateCategoryTestDataGenerator.GetCategoriesToUpdate),
+    parameters: 5,
+    MemberType = typeof(UpdateCategoryTestDataGenerator)
+    )]
+    public async Task UpdateCategoryOnlyName(Category exampleCategory, UpdateCategoryInput exampleInput)
+    {
+        var input = new UpdateCategoryInput(
+            exampleInput.Id,
+            exampleInput.Name
+        );
+
+        var dbContext = fixture.CreateDbContext();
+        await dbContext.AddRangeAsync(fixture.GetExampleCategoryList());
+        var trackingInfo = await dbContext.AddAsync(exampleCategory);
+        await dbContext.SaveChangesAsync();
+        trackingInfo.State = EntityState.Detached;
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var useCase = new UpdateCategoryUseCase(repository, unitOfWork);
+
+        CategoryModelOutput output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(exampleCategory.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive);
+        var dbCategory = await fixture
+           .CreateDbContext(preserveData: true)
+           .Categories.SingleAsync((x) => x.Id == input.Id, CancellationToken.None);
+        dbCategory.Should().NotBeNull();
+        dbCategory.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(exampleCategory.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive);
+        dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+    }
+
+    [Theory(DisplayName = nameof(UpdateCategoryWithoutProvidingIsActive))]
+    [Trait("Integration/Application", "UpdateCategory - Use Cases")]
+    [MemberData(
+        nameof(UpdateCategoryTestDataGenerator.GetCategoriesToUpdate),
+        parameters: 5,
+        MemberType = typeof(UpdateCategoryTestDataGenerator)
+    )]
+    public async Task UpdateCategoryWithoutProvidingIsActive(Category exampleCategory, UpdateCategoryInput exampleInput)
+    {
+        var input = new UpdateCategoryInput(
+            exampleInput.Id,
+            exampleInput.Name,
+            exampleInput.Description
+        );
+
+        var dbContext = fixture.CreateDbContext();
+        await dbContext.AddRangeAsync(fixture.GetExampleCategoryList());
+        var trackingInfo = await dbContext.AddAsync(exampleCategory);
+        await dbContext.SaveChangesAsync();
+        trackingInfo.State = EntityState.Detached;
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var useCase = new UpdateCategoryUseCase(repository, unitOfWork);
+
+        CategoryModelOutput output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive);
+        var dbCategory = await fixture
+           .CreateDbContext(preserveData: true)
+           .Categories.SingleAsync((x) => x.Id == input.Id, CancellationToken.None);
+        dbCategory.Should().NotBeNull();
+        dbCategory.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive);
+        dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+    }
+
+    [Theory(DisplayName = nameof(ThrowWhenCantUpdateCategory))]
+    [Trait("Integration/Application", "UpdateCategory - Use Cases")]
+    [MemberData(
+        nameof(UpdateCategoryTestDataGenerator.GetInvalidInputs),
+        parameters: 6,
+        MemberType = typeof(UpdateCategoryTestDataGenerator)
+    )]
+    public async Task ThrowWhenCantUpdateCategory(UpdateCategoryInput input, string exceptionMessage)
+    {
+        var exampleCategory = fixture.GetExampleCategory();
+        var dbContext = fixture.CreateDbContext();
+        await dbContext.Categories.AddAsync(exampleCategory);
+        await dbContext.SaveChangesAsync();
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        input.Id = exampleCategory.Id;
+        await dbContext.Categories.AddAsync(exampleCategory);
+
+        var useCase = new UpdateCategoryUseCase(repository, unitOfWork);
+
+        var act = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await act.Should()
+            .ThrowAsync<EntityValidationException>()
+            .WithMessage(exceptionMessage);
+    }
+}
---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.IntegrationTests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Categories/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -0,0 +1,44 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Categories.UpdateCategory;
+using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.Common;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.UpdateCategory;
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTestFixtureCollection : ICollectionFixture<UpdateCategoryTestFixture> { }
+
+public class UpdateCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+    public UpdateCategoryInput GetValidInput(Guid? id = null)
+        => new(
+            id ?? Guid.NewGuid(),
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean());
+
+    public UpdateCategoryInput GetInvalidInputShortName()
+    {
+        var input = GetValidInput();
+        input.Name = input.Name[..2];
+        return input;
+    }
+
+    public UpdateCategoryInput GetInvalidInputTooLongName()
+    {
+        var input = GetValidInput();
+        var tooLongNameForCategory = Faker.Commerce.ProductName();
+        while (tooLongNameForCategory.Length <= 255)
+            tooLongNameForCategory += Faker.Commerce.ProductName();
+        input.Name = tooLongNameForCategory;
+        return input;
+    }
+
+    public UpdateCategoryInput GetInvalidInputTooLongDescription()
+    {
+        var input = GetValidInput();
+        var tooLongDescriptionForCategory = Faker.Commerce.ProductDescription();
+        while (tooLongDescriptionForCategory.Length <= 10_000)
+            tooLongDescriptionForCategory += Faker.Commerce.ProductDescription();
+        input.Description = tooLongDescriptionForCategory;
+        return input;
+    }
+}
