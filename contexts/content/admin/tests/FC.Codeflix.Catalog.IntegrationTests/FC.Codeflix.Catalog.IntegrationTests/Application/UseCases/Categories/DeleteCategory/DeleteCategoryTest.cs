1ff5e48df999d424faabc81dcc81a112b278abf4
Rafael Freitas <raffreitasx@gmail.com>
2025-04-09 20:49:18 -0300

    Add tests for DeleteCategory use case
    Updated `DeleteCategoryTest.cs` to include tests for
    the `DeleteCategory` use case, adding methods to verify
    successful deletion and handling of non-existent categories.
    Modified `DeleteCategoryTestFixture.cs` to define a
    collection for the test fixture, ensuring proper setup
    for the tests.

---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.IntegrationTests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Categories/DeleteCategory/DeleteCategoryTest.cs
@@ -0,0 +1,73 @@
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Application.UseCases.Categories.DeleteCategory;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+
+using FluentAssertions;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTest(DeleteCategoryTestFixture fixture)
+{
+    [Fact(DisplayName = nameof(DeleteCategory))]
+    [Trait("Integration/Application", "DeleteCategory - Use Cases")]
+    public async Task DeleteCategory()
+    {
+        var categoryExample = fixture.GetExampleCategory();
+        var categoryExampleList = fixture.GetExampleCategoryList();
+        var dbContext = fixture.CreateDbContext();
+        await dbContext.Categories.AddRangeAsync(categoryExampleList);
+        var trackingInfo = await dbContext.Categories.AddAsync(categoryExample);
+        await dbContext.SaveChangesAsync();
+        trackingInfo.State = EntityState.Detached;
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+
+        var input = new DeleteCategoryInput(categoryExample.Id);
+        var useCase = new DeleteCategoryUseCase(repository, unitOfWork);
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        var dbCategory = await fixture
+            .CreateDbContext(preserveData: true)
+            .Categories
+            .SingleOrDefaultAsync((x) => x.Id == categoryExample.Id);
+        dbCategory.Should().BeNull();
+
+        var dbCount = await fixture
+            .CreateDbContext(preserveData: true)
+            .Categories
+            .CountAsync();
+        dbCount.Should().Be(categoryExampleList.Count);
+    }
+
+    [Fact(DisplayName = nameof(ThrowWhenCategoryNotFound))]
+    [Trait("Integration/Application", "DeleteCategory - Use Cases")]
+    public async Task ThrowWhenCategoryNotFound()
+    {
+        var exampleGuid = Guid.Empty;
+        var dbContext = fixture.CreateDbContext();
+        var trackingInfo = await dbContext.Categories.AddAsync(fixture.GetExampleCategory());
+        await dbContext.SaveChangesAsync();
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+
+        var input = new DeleteCategoryInput(exampleGuid);
+        var useCase = new DeleteCategoryUseCase(repository, unitOfWork);
+
+        var act = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await act.Should()
+            .ThrowAsync<NotFoundException>();
+
+        var dbCount = await fixture
+            .CreateDbContext(preserveData: true)
+            .Categories
+            .CountAsync();
+
+        dbCount.Should().Be(1);
+    }
+}
---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.IntegrationTests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Categories/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.Common;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture>
+{
+}
+public class DeleteCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+}
