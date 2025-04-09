b635f8aed32f9fa3f82e9899d15dd175c8fed3bb
Rafael Freitas <raffreitasx@gmail.com>
2025-04-09 19:41:23 -0300

    Add category use cases base fixture and update tests
    Introduce `CategoryUseCasesBaseFixture` for generating valid category data. Update `GetCategoryTest` to utilize this fixture for creating example categories and handling exceptions. Modify `GetCategoryTestFixture` to inherit from the new base fixture for shared setup across tests.

---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.IntegrationTests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Categories/Common/CategoryUseCasesBaseFixture.cs
@@ -0,0 +1,36 @@
+﻿using FC.Codeflix.Catalog.Domain.Entities;
+using FC.Codeflix.Catalog.IntegrationTests.Base;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.Common;
+public class CategoryUseCasesBaseFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = string.Empty;
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean()
+        => Faker.Random.Bool();
+
+    public Category GetExampleCategory()
+      => new(
+          GetValidCategoryName(),
+          GetValidCategoryDescription(),
+          GetRandomBoolean());
+
+    public List<Category> GetExampleCategoryList(int length = 10)
+      => [.. Enumerable.Range(0, length).Select(_ => GetExampleCategory())];
+}
---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.IntegrationTests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Categories/GetCategory/GetCategoryTest.cs
@@ -0,0 +1,55 @@
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Application.UseCases.Categories.GetCategory;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryTest(GetCategoryTestFixture fixture)
+{
+    [Fact(DisplayName = nameof(GetCategory))]
+    [Trait("Integration/Application", "GetCategory - Use Cases")]
+    public async Task GetCategory()
+    {
+        var exampleCategory = fixture.GetExampleCategory();
+        var dbContext = fixture.CreateDbContext();
+        var categoryRepository = new CategoryRepository(dbContext);
+        await dbContext.Categories.AddAsync(exampleCategory);
+        await dbContext.SaveChangesAsync();
+        var input = new GetCategoryInput(exampleCategory.Id);
+        var useCase = new GetCategoryUseCase(categoryRepository);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(exampleCategory.Id);
+        output.Name.Should().Be(exampleCategory.Name);
+        output.Description.Should().Be(exampleCategory.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive);
+        output.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+    }
+
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExists))]
+    [Trait("Application", "GetCategory - Use Cases")]
+    public async Task NotFoundExceptionWhenCategoryDoesntExists()
+    {
+        var exampleGuid = Guid.NewGuid();
+        var exampleCategory = fixture.GetExampleCategory();
+        var dbContext = fixture.CreateDbContext();
+        await dbContext.Categories.AddAsync(exampleCategory);
+        await dbContext.SaveChangesAsync();
+
+        var repository = new CategoryRepository(dbContext);
+
+        var input = new GetCategoryInput(exampleGuid);
+        var useCase = new GetCategoryUseCase(repository);
+
+        var act = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await act.Should()
+            .ThrowAsync<NotFoundException>()
+            .WithMessage($"Category '{exampleGuid}' not found.");
+    }
+}
