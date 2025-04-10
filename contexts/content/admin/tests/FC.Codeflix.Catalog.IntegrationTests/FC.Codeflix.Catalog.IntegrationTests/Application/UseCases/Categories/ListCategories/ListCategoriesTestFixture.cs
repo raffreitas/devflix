4416fbc41f01b6146891083051c3b1984dc01d12
Rafael Freitas <raffreitasx@gmail.com>
2025-04-09 21:03:32 -0300

    Add integration tests for ListCategoriesUseCase
    Introduce `ListCategoriesTest` with unit tests for listing categories, including scenarios for total counts, empty persistence, pagination, text search, and ordering. Create `ListCategoriesTestFixture` to generate example category lists and support ordered list cloning, enhancing the test suite for category listing functionality.

---
+++ contexts/content/admin/tests/FC.Codeflix.Catalog.IntegrationTests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Categories/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,36 @@
+﻿using FC.Codeflix.Catalog.Domain.Entities;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearcheableRepository;
+using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.Common;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Categories.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCategoriesTestFixture>
+{
+}
+
+public class ListCategoriesTestFixture : CategoryUseCasesBaseFixture
+{
+    public List<Category> GetExampleCategoryListWithNames(IEnumerable<string> names)
+      => names.Select(name =>
+      {
+          var category = GetExampleCategory();
+          category.Update(name);
+          return category;
+      }).ToList();
+
+    public List<Category> CloneCategoriesListOrdered(List<Category> categoriesList, string orderBy, SearchOrder order)
+    {
+        var listClone = new List<Category>(categoriesList);
+        return (orderBy.ToLower(), order) switch
+        {
+            ("name", SearchOrder.Asc) => [.. listClone.OrderBy(x => x.Name)],
+            ("name", SearchOrder.Desc) => [.. listClone.OrderByDescending(x => x.Name)],
+            ("id", SearchOrder.Asc) => [.. listClone.OrderBy(x => x.Id)],
+            ("id", SearchOrder.Desc) => [.. listClone.OrderByDescending(x => x.Id)],
+            ("createdat", SearchOrder.Asc) => [.. listClone.OrderBy(x => x.CreatedAt)],
+            ("createdat", SearchOrder.Desc) => [.. listClone.OrderByDescending(x => x.CreatedAt)],
+            _ => [.. listClone.OrderBy(x => x.Name)],
+        };
+    }
+}
